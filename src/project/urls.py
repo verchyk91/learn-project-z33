2eaebf14b91400e5d82e807b2883f5ac5f277bb1
Dmitry <verchyk91@mail.ru>
2020-10-02 20:34:38 +0300

    django1.1

--- src/project/urls.py
+++ src/project/urls.py
@@ -7,6 +7,7 @@ from django.urls import include
 from django.urls import path
 
 from applications.hello.views import index
+from applications.hello.views import update
 
 
 def view_index(request: HttpRequest):
