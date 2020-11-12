ade0734f31591fec0e6108ecaa0da498886cbd5e
Dmitry <verchyk91@mail.ru>
2020-11-12 15:46:09 +0300

    bot1

--- src/project/settings.py
+++ src/project/settings.py
@@ -1,115 +1,20 @@
-# import os
-# from pathlib import Path
-#
-# import dj_database_url
-# from dynaconf import settings as _ds
-#
-# REPO_DIR = Path(__file__).resolve().parent.parent.parent
-# BASE_DIR = REPO_DIR / "src"
-# PROJECT_DIR = BASE_DIR / "project"
-#
-# SECRET_KEY = _ds.SECRET_KEY
-#
-# DEBUG = _ds.DEBUG
-#
-# ALLOWED_HOSTS = _ds.ALLOWED_HOSTS + ["localhost", "127.0.0.1"]
-#
-# INSTALLED_APPS = [
-#     "django.contrib.admin",
-#     "django.contrib.auth",
-#     "django.contrib.contenttypes",
-#     "django.contrib.sessions",
-#     "django.contrib.messages",
-#     "django.contrib.staticfiles",
-#     # -------------------------
-#     "applications.hello.apps.HelloConfig",
-#     "applications.home.apps.HomeConfig",
-# ]
-#
-# MIDDLEWARE = [
-#     "django.middleware.security.SecurityMiddleware",
-#     "whitenoise.middleware.WhiteNoiseMiddleware",
-#     "django.contrib.sessions.middleware.SessionMiddleware",
-#     "django.middleware.common.CommonMiddleware",
-#     "django.middleware.csrf.CsrfViewMiddleware",
-#     "django.contrib.auth.middleware.AuthenticationMiddleware",
-#     "django.contrib.messages.middleware.MessageMiddleware",
-#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-# ]
-#
-# ROOT_URLCONF = "project.urls"
-#
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [
-#             PROJECT_DIR / "templates",
-#         ],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
-#
-# WSGI_APPLICATION = "project.wsgi.application"
-#
-# database_url = os.getenv("DATABASE_URL", _ds.DATABASE_URL)
-#
-# DATABASES = {
-#     "default": dj_database_url.parse(database_url),
-# }
-#
-#
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#     },
-# ]
-#
-#
-# LANGUAGE_CODE = "en-us"
-#
-# TIME_ZONE = "UTC"
-#
-# USE_I18N = True
-#
-# USE_L10N = True
-#
-# USE_TZ = True
-#
-# STATIC_URL = "/s/"
-# STATICFILES_DIRS = [
-#     PROJECT_DIR / "static",
-# ]
-# STATIC_ROOT = REPO_DIR / ".static"
-# if not DEBUG:
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#
-#
-
 import os
 from pathlib import Path
 
 import dj_database_url
+import sentry_sdk
 from dynaconf import settings as _ds
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = _ds.DEBUG
 
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=_ds.SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+    )
 
 REPO_DIR = Path(__file__).resolve().parent.parent.parent
 BASE_DIR = REPO_DIR / "src"
@@ -117,7 +22,9 @@ PROJECT_DIR = BASE_DIR / "project"
 
 SECRET_KEY = _ds.SECRET_KEY
 
-ALLOWED_HOSTS = _ds.ALLOWED_HOSTS + ["*"]
+HOST = _ds.HOST
+
+ALLOWED_HOSTS = list(set(_ds.ALLOWED_HOSTS + ["localhost", "127.0.0.1", HOST]))
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -126,6 +33,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # -------------------------
+    "applications.blog.apps.BlogConfig",
+    "applications.bots.apps.BotsConfig",
     "applications.hello.apps.HelloConfig",
     "applications.home.apps.HomeConfig",
 ]
@@ -147,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            f"{PROJECT_DIR}/templates",
+            PROJECT_DIR / "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -201,3 +111,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = REPO_DIR / ".static"
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+TG_BOT_TOKEN = _ds.TG_BOT_TOKEN
--- src/project/urls.py
+++ src/project/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from django.urls import include
 from django.urls import path
 
@@ -11,5 +12,7 @@ urlpatterns = [
     path("", include("applications.home.urls")),
     path("admin/", admin.site.urls),
     path("hello/", include("applications.hello.urls")),
+    path("blog/", include("applications.blog.urls")),
     path("sentry-debug/", trigger_error),
+
 ]
